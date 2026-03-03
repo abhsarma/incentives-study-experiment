ff195bb9719c1c1d986cae769d5b85bf106ed147
Abhraneel Sarma <abhraneelsarma2024@u.northwestern.edu>
2026-03-03 10:43:50 +0530

    training fix

--- src/public/incentives-dm/assets/03-training.tsx
+++ src/public/incentives-dm/assets/03-training.tsx
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
-import {useCallback, useEffect, useMemo, useState} from 'react';
-import { StoredAnswer, StimulusParams } from '../../../store/types';
+import {useEffect} from 'react';
+import {StimulusParams } from '../../../store/types';
 
 // This React component renders a bar chart with 5 bars and 2 of them highlighted by dots.
 // The data value comes from the config file and pass to this component by parameters.
@@ -10,9 +10,11 @@ function DisplayTrial({ parameters }: StimulusParams<any>) {
     const trainingIdx = "training-" + index;
     const imgURL = `../incentives-dm/assets/img/training/0${index}-training-${vis}.jpg`;
 
-    console.log(imgURL, index, vis, target);
-
-    d3.select("#target").html(target);
+    useEffect(() => {
+        setTimeout(() => {
+            d3.select("#target").html(target);
+        }, 100)
+    }, []);
 
     return (
         <div className="chart-wrapper">
@@ -27,22 +29,3 @@ function DisplayTrial({ parameters }: StimulusParams<any>) {
 }
 
 export default DisplayTrial;
-
-
-{/* <script>
-    Revisit.onDataReceive((data) => {
-        const index = data['index'];
-        const vis = data['vis'];
-        const target = data['target'];
-        let imgStr = "./img/training/0" + index + "-training-ci.jpg";
-
-        d3.select("span.insert-quantile").text(target);
-        d3.select("img").attr("src", imgStr)
-    });
-
-    console.log(document.getElementById("target"))
-
-    // Call out that 'barChart' needs to match ID in 'response' object
-    // Revisit.postAnswers({ trainingProb: userAnswer });
-</script> */}
-    
